93b906d84931931f330fd6a0182ea7f334e8de30
Eric Valois <info@ericvalois.com>
2021-01-04 08:52:07 -0500

    Add new global data file and use it to display the analytic code

---
+++ src/_data/meta.js
@@ -0,0 +1,8 @@
+module.exports = {
+	url: process.env.URL || "http://localhost:8080",
+	siteName: "Eric Valois",
+	siteDescription: "A personal website to play with new web technologies and express myself.",
+	authorName: "Eric Valois",
+	twitterUsername: "ericvalois",
+	env: process.env.ELEVENTY_ENV,
+};
